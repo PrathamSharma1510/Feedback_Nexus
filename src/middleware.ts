b40ba0317879cae46e70c3e4a47c95bae2724cd7
Pratham Sharma <62831943+PrathamSharma1510@users.noreply.github.com>
2024-08-06 17:47:01 +0530

    rest bugs

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,31 +1,30 @@
-import { NextRequest, NextResponse } from "next/server";
-import { getToken } from "next-auth/jwt";
-export { default } from "next-auth/middleware";
+import { NextRequest, NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+export { default } from 'next-auth/middleware';
+
 export const config = {
-  matcher: ["/dashboard/:path*", "/sign-in", "/sign-up", "/", "/verify/:path*"],
+  matcher: ['/dashboard/:path*', '/sign-in', '/sign-up', '/', '/verify/:path*'],
 };
 
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
-  const currentUrl = request.nextUrl.clone(); // Clone to safely modify if needed
+  const url = request.nextUrl;
 
-  // Define paths that should redirect to the dashboard if the user is authenticated
-  const publicPaths = ["/sign-in", "/sign-up", "/verify", "/"];
-  const isAuthenticated = token !== null;
-
-  // Redirect authenticated users trying to access public paths to the dashboard
+  // Redirect to dashboard if the user is already authenticated
+  // and trying to access sign-in, sign-up, or home page
   if (
-    isAuthenticated &&
-    publicPaths.some((path) => currentUrl.pathname.startsWith(path))
+    token &&
+    (url.pathname.startsWith('/sign-in') ||
+      url.pathname.startsWith('/sign-up') ||
+      url.pathname.startsWith('/verify') ||
+      url.pathname === '/')
   ) {
-    return NextResponse.redirect(new URL("/dashboard", request.url));
+    return NextResponse.redirect(new URL('/dashboard', request.url));
   }
 
-  // Redirect unauthenticated users trying to access protected paths to the sign-in page
-  if (!isAuthenticated && currentUrl.pathname.startsWith("/dashboard")) {
-    return NextResponse.redirect(new URL("/sign-in", request.url));
+  if (!token && url.pathname.startsWith('/dashboard')) {
+    return NextResponse.redirect(new URL('/sign-in', request.url));
   }
 
-  // Continue with the normal flow for all other requests
   return NextResponse.next();
 }
