0b549dab4b4f1765eff33d9b16298e95bcaef5f6
Pratham Sharma <62831943+PrathamSharma1510@users.noreply.github.com>
2024-08-07 04:21:25 +0530

    favicon updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,21 +3,19 @@ import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster";
 import AuthProvider from "@/context/AuthProvider";
 import "./globals.css";
+
 const inter = Inter({ subsets: ["latin"] });
-export const metadata = {
+
+export const metadata: Metadata = {
   title: "Feedback Nexuss",
   description: "Your feedback management system",
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon.ico",
-  },
 };
 
-interface RootLayoutProps {
+export default function RootLayout({
+  children,
+}: Readonly<{
   children: React.ReactNode;
-}
-
-export default async function RootLayout({ children }: RootLayoutProps) {
+}>) {
   return (
     <html lang="en" className="dark">
       <AuthProvider>
