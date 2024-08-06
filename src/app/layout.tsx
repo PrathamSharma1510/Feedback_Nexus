b5e1fdb95741c2dd116cff40325469ca16b92e25
Pratham Sharma <62831943+PrathamSharma1510@users.noreply.github.com>
2024-08-06 17:34:24 +0530

    middleare and layout page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster";
 import AuthProvider from "@/context/AuthProvider";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "True Feedback",
